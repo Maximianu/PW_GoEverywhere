48b27a570b9b3ecea02e7ed850261c3befdfff91
David3871 <davidcoelho387@gmail.com>
2026-05-24 17:48:40 +0100

    correçoes

--- GoEverywhere/types/generated/contentTypes.d.ts
+++ GoEverywhere/types/generated/contentTypes.d.ts
@@ -107,43 +107,6 @@ export interface AdminApiTokenPermission extends Struct.CollectionTypeSchema {
   };
 }
 
-export interface AdminAuditLog extends Struct.CollectionTypeSchema {
-  collectionName: 'strapi_audit_logs';
-  info: {
-    displayName: 'Audit Log';
-    pluralName: 'audit-logs';
-    singularName: 'audit-log';
-  };
-  options: {
-    draftAndPublish: false;
-    timestamps: false;
-  };
-  pluginOptions: {
-    'content-manager': {
-      visible: false;
-    };
-    'content-type-builder': {
-      visible: false;
-    };
-  };
-  attributes: {
-    action: Schema.Attribute.String & Schema.Attribute.Required;
-    createdAt: Schema.Attribute.DateTime;
-    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
-      Schema.Attribute.Private;
-    date: Schema.Attribute.DateTime & Schema.Attribute.Required;
-    locale: Schema.Attribute.String & Schema.Attribute.Private;
-    localizations: Schema.Attribute.Relation<'oneToMany', 'admin::audit-log'> &
-      Schema.Attribute.Private;
-    payload: Schema.Attribute.JSON;
-    publishedAt: Schema.Attribute.DateTime;
-    updatedAt: Schema.Attribute.DateTime;
-    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
-      Schema.Attribute.Private;
-    user: Schema.Attribute.Relation<'oneToOne', 'admin::user'>;
-  };
-}
-
 export interface AdminPermission extends Struct.CollectionTypeSchema {
   collectionName: 'admin_permissions';
   info: {
@@ -685,6 +648,10 @@ export interface ApiClienteCliente extends Struct.CollectionTypeSchema {
       'api::cliente.cliente'
     > &
       Schema.Attribute.Private;
+    pedidos: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::pedido-mission.pedido-mission'
+    >;
     PrimeiroNome: Schema.Attribute.String;
     publishedAt: Schema.Attribute.DateTime;
     UltimoNome: Schema.Attribute.String;
@@ -896,11 +863,19 @@ export interface ApiPedidoMissionPedidoMission
   };
   attributes: {
     bilhete: Schema.Attribute.Relation<'oneToOne', 'api::bilhete.bilhete'>;
+    cliente: Schema.Attribute.Relation<'oneToOne', 'api::cliente.cliente'>;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     Estado: Schema.Attribute.Enumeration<
-      ['Pendente', 'Aprovado', 'Rejeitado', 'Transito', 'Concluido']
+      [
+        'Pendente',
+        'Aprovado',
+        'Rejeitado',
+        'Transito',
+        'Concluido',
+        'Cancelado',
+      ]
     >;
     estafeta: Schema.Attribute.Relation<'oneToOne', 'api::estafeta.estafeta'>;
     estafeta2: Schema.Attribute.Relation<'manyToOne', 'api::estafeta.estafeta'>;
@@ -915,6 +890,7 @@ export interface ApiPedidoMissionPedidoMission
     Prioridade_Entrega: Schema.Attribute.Enumeration<
       ['Alta', 'M\u00E9dia', 'Baixa']
     >;
+    Problema: Schema.Attribute.String;
     publishedAt: Schema.Attribute.DateTime;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
@@ -1460,7 +1436,6 @@ declare module '@strapi/strapi' {
     export interface ContentTypeSchemas {
       'admin::api-token': AdminApiToken;
       'admin::api-token-permission': AdminApiTokenPermission;
-      'admin::audit-log': AdminAuditLog;
       'admin::permission': AdminPermission;
       'admin::role': AdminRole;
       'admin::session': AdminSession;
